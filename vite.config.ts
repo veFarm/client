a34f6f89775b112d2aad8ebc8aef8da5ca873a7b
fede-rodes <federodes@gmail.com>
2023-04-13 19:36:38 +0200

    build working

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import path from "node:path";
-// import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+// You don't need to add this to deps, it's included by @esbuild-plugins/node-modules-polyfill
+import rollupNodePolyFills from "rollup-plugin-node-polyfills";
 // import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
@@ -13,7 +16,13 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve("./src"),
+      // This Rollup aliases are extracted from @esbuild-plugins/node-modules-polyfill,
+      // see https://github.com/remorses/esbuild-plugins/blob/master/node-modules-polyfill/src/polyfills.ts
+      // process and buffer are excluded because already managed
+      // by node-globals-polyfill
       // "./runtimeConfig": "./runtimeConfig.browser",
+      buffer: "rollup-plugin-node-polyfills/polyfills/buffer-es6",
+      crypto: "crypto-browserify",
     },
   },
   optimizeDeps: {
@@ -23,11 +32,23 @@ export default defineConfig({
         global: "globalThis",
       },
       // Enable esbuild polyfill plugins
-      // plugins: [
-      //   NodeGlobalsPolyfillPlugin({
-      //     buffer: true,
-      //   }),
-      // ],
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        // Enable rollup polyfills plugin
+        // used during production bundling
+        // @ts-ignore
+        rollupNodePolyFills({ crypto: true }),
+      ],
     },
   },
 });
