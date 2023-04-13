fb5f933dc3d38cc49671b8301a67024a3e18e77a
fede-rodes <federodes@gmail.com>
2023-04-13 18:35:26 +0200

    cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import path from "path";
+import path from "node:path";
 // import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 // import { visualizer } from 'rollup-plugin-visualizer';
 
