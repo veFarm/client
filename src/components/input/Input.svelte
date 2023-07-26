7643653298d2e17aa1b9d7f72905affae61b6690
fede-rodes <federodes@gmail.com>
2023-07-27 10:39:43 +1200

    adding input stories

--- src/components/input/Input.svelte
+++ src/components/input/Input.svelte
@@ -2,15 +2,15 @@
   import type { HTMLInputAttributes } from "svelte/elements";
 
   type $$Props = HTMLInputAttributes & {
-    value: any;
-    label?: string;
     id: string;
+    label?: string;
+    value: any;
     error?: string;
   };
 
-  export let value: any;
-  export let label = "";
   export let id: string;
+  export let label = "";
+  export let value: any;
   export let error = "";
 
   let hasError = false;
@@ -18,13 +18,14 @@
   $: hasError = error != null && error.length > 0;
 </script>
 
-<label for={id} class="text-background text-sm" class:text-danger={hasError}>
+<label for={id} class="{hasError ? 'text-danger' : 'text-background'} text-sm">
   {label}
   <div class="relative">
     <input
       {id}
-      class="bg-gray-50 border border-gray-300 text-background text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 disabled:bg-gray-100"
-      class:border-danger={hasError}
+      class="bg-gray-50 border {hasError
+        ? 'border-danger'
+        : 'border-gray-300'} text-background text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 disabled:bg-gray-100"
       {...$$restProps}
       bind:value
       on:input
---
+++ src/components/input/stories/Input.story.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import type { ComponentProps } from "svelte";
+  import { Input } from "..";
+
+  type $$Props = ComponentProps<Input>;
+</script>
+
+<Input id="test" label="I'm the label" value="Some value" {...$$props} />
