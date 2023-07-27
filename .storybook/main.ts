ceafb67ceb3411a695e6f4280cafcf82ecd1fa92
fede-rodes <federodes@gmail.com>
2023-07-28 11:55:40 +1200

    bumping storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,13 @@
 import type { StorybookConfig } from "@storybook/svelte-vite";
-
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
   framework: {
     name: "@storybook/svelte-vite",
-    options: {},
+    options: {}
   },
   docs: {
-    autodocs: "tag",
-  },
+    autodocs: "tag"
+  }
 };
 export default config;
