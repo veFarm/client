31dd1234b1a7c4f9ae7e5d94b04be27603cc156c
fede-rodes <federodes@gmail.com>
2023-08-01 21:22:23 +1200

    adding stories for StatItem

---
+++ src/components/stat-item/StatItem.stories.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+  import { StatItem } from ".";
+</script>
+
+<Meta title="components/StatItem" component={StatItem} />
+
+<Template let:args>
+  <div class="bg-background p-4">
+    <StatItem {...args} />
+  </div>
+</Template>
+
+<Story
+  name="Default"
+  args={{
+    value: 345,
+    label: "Users",
+  }}
+/>
--- src/components/stat-item/StatItem.svelte
+++ src/components/stat-item/StatItem.svelte
@@ -3,13 +3,11 @@
   export let label = "";
 </script>
 
-<div class="text-center">
-  <h6 class="text-2xl font-bold lg:text-3xl xl:text-4xl text-gray-300">
+<div class="text-center text-gray-300">
+  <h6 class="text-2xl font-bold lg:text-3xl xl:text-4xl">
     {value}
   </h6>
-  <p
-    class="text-sm font-medium tracking-widest text-gray-300 uppercase lg:text-base"
-  >
+  <p class="text-sm font-medium tracking-widest uppercase lg:text-base">
     {label}
   </p>
 </div>
