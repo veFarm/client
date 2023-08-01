66fac9751733774518a14614f5191df767427d2f
fede-rodes <federodes@gmail.com>
2023-08-01 21:13:43 +1200

    adding stories for Input

---
+++ src/components/input/Input.stories.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+  import { Input } from ".";
+</script>
+
+<Meta title="components/Input" component={Input} />
+
+<Template let:args>
+  <div class="bg-body p-2">
+    <Input id="test" label="I'm the label" value="Some value" {...args} />
+  </div>
+</Template>
+
+<Story name="Default" args={{}} />
+
+<Story
+  name="Error"
+  args={{
+    error: "I'm the error message",
+  }}
+/>
--- src/components/input/stories/Input.story.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import type { ComponentProps } from "svelte";
-  import { Input } from "..";
-
-  type $$Props = ComponentProps<Input>;
-</script>
-
-<Input id="test" label="I'm the label" value="Some value" {...$$props} />
