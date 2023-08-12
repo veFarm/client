615214e77f3186aa15e3cc4927a7da26c1fa8cd3
fede-rodes <federodes@gmail.com>
2023-08-12 15:18:12 +1200

    add story for RevokeAllowanceButton

--- src/components/revoke-allowance-button/RevokeAllowanceButton.stories.svelte
+++ src/components/revoke-allowance-button/RevokeAllowanceButton.stories.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
   import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
-  import { AllowanceButton } from ".";
+  import { RevokeAllowanceButton } from ".";
 </script>
 
-<Meta title="components/AllowanceButton" component={AllowanceButton} />
+<Meta title="components/AllowanceButton" component={RevokeAllowanceButton} />
 
 <Story name="Default">
   <div class="bg-body p-4 max-w-lg mx-auto">
-    <AllowanceButton />
+    <RevokeAllowanceButton />
   </div>
 </Story>
