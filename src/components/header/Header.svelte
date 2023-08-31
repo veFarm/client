5c5f2b58a67d39cba9c7a48abb86890d3c32afdb
fede-rodes <federodes@gmail.com>
2023-08-31 12:04:21 +1200

    Disconnect wallet

--- src/components/header/Header.svelte
+++ src/components/header/Header.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
+  import { Popover } from "svelte-smooth-popover";
   import { wallet } from "@/stores/wallet";
   import { shortenAddress } from "@/utils/shorten-address";
   import { formatUnits } from "@/utils/format-units";
+  import ChevronDown from "@/assets/ChevronDown.svelte";
+
+  /** Popover state. */
+  let isOpen = false;
+
+  function handleDisconnect() {
+    localStorage.removeItem("user");
+    wallet.disconnect();
+    isOpen = false;
+  }
 </script>
 
 <nav
@@ -10,7 +21,7 @@
     bg-background border-b border-muted
     px-2 sm:px-4 py-2
     flex flex-wrap items-center justify-between
-    text-sm md:text-base
+    text-sm text-gray-300 md:text-base
   "
 >
   <a href="/">
@@ -21,10 +32,30 @@
   {#if $wallet.account == null}
     <!-- <ConnectWalletButton size="small" /> -->
   {:else}
-    <span>
-      {formatUnits($wallet.balance.vet, 2)}&nbsp;VET{" "}
-      {shortenAddress($wallet.account)}
-    </span>
+    <div>
+      {formatUnits($wallet.balance.vet, 2)}&nbsp;VET&nbsp;
+
+      <button class="inline-block ">
+        {shortenAddress($wallet.account)}
+        <ChevronDown class="inline-block text-inherit" />
+
+        <Popover
+          showOnClick
+          hideOnExternalClick
+          caretWidth={0}
+          on:open={() => {
+            isOpen = true;
+          }}
+          on:close={() => {
+            isOpen = false;
+          }}
+        >
+          <div class="bg-highlight border border-muted rounded-md px-4 py-3">
+            <button class="text-sm md:text-base" on:click={handleDisconnect}>Disconnect Wallet</button>
+          </div>
+        </Popover>
+      </button>
+    </div>
   {/if}
 </nav>
 
