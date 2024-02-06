9f423ca3e993d9cb014592f0dbe5e86f216fc41b
fede-rodes <federodes@gmail.com>
2024-02-06 17:01:48 +1300

    add start server step

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
     },
   },
   e2e: {
-    baseUrl: "http://localhost:5173/",
+    // baseUrl: "http://localhost:5173/",
+    baseUrl: "http://localhost:4173/",
     viewportWidth: 1440,
     viewportHeight: 900,
     setupNodeEvents(on, config) {
