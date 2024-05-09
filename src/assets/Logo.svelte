b5365bcaf6db6604ce3c88c94172aba0ff307272
fede-rodes <federodes@gmail.com>
2024-05-09 15:37:10 +1200

    logo

--- src/assets/Logo.svelte
+++ src/assets/Logo.svelte
@@ -3,12 +3,15 @@
 </script>
 
 <div class="text-xl sm:text-2xl font-bold" {...$$props}>
-  <img src={zap} class="inline -mt-1" />
-  <span class="text-slate-300">V</span><span class="text-primary">earn</span>
+  <img src={zap} class="inline -mt-1 -mr-1" />
+  <span class="v-color">V</span><span class="text-primary">earn</span>
   <!-- <span>Vearn</span> -->
 </div>
 
 <style>
+  .v-color {
+    color: rgb(178, 178, 178);
+  }
   /* span {
     background: rgb(255,255,255);
     background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,229,0,1) 60%);
