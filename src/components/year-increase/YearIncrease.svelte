7907aa3f4c5811c8e697fb90281826f2840ca8cd
fede-rodes <federodes@gmail.com>
2024-07-17 11:18:09 +1200

    add yearly increase

---
+++ src/components/year-increase/YearIncrease.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  export let value: string;
+</script>
+
+  <table class="w-full text-sm font-medium" data-cy="year-increase">
+    <tbody>
+      <tr >
+        <td class="title">
+          Est. yearly increase:
+        </td>
+        <td class="value">
+          {value} VET
+        </td>
+      </tr>
+    </tbody>
+  </table>
+
+<style lang="postcss">
+  .title {
+    @apply w-0 whitespace-nowrap font-normal;
+  }
+  .value {
+    @apply w-1/2 truncate text-right text-accent;
+    max-width: 1px;
+  }
+  .border-style {
+    @apply border-l-2 border-muted ml-2.5 mr-1 pl-3;
+  }
+  tr > td {
+    @apply pb-2;
+  }
+</style>
